10901bccf7c9c252e061a0e76024a0d51d16d061
kvnwdev <47703820+kvnwdev@users.noreply.github.com>
2026-07-24 12:04:36 -0400

    Update bundled formula to 0.4.4

--- Formula/protoncli.rb
+++ Formula/protoncli.rb
@@ -1,8 +1,8 @@
 class Protoncli < Formula
   desc "Production-ready CLI email client for ProtonMail Bridge"
   homepage "https://github.com/kvnwdev/protoncli"
-  url "https://github.com/kvnwdev/protoncli/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "11b257146c3b43fd673d21ee9a62cc27dce5929a1fcab333c9b8c7703ef51706"
+  url "https://github.com/kvnwdev/protoncli/archive/refs/tags/v0.4.4.tar.gz"
+  sha256 "8c45a8c6700d0811e07da0bef214f88ca39456bdcf12da7212c0061e480e853f"
   license "MIT"
 
   depends_on "rust" => :build
